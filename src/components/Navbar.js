b6def1a3adf1e15fe577cb6cc38ad3dd33234b75
Juan Diego <jdestradap@gmail.com>
2019-12-26 15:36:37 -0500

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ https://www.iconfinder.com/Makoto_msk */}
         </Link>
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
-            <Link to="/" className="nav-link">
+            <Link to="/" className="nav-link" id="products-home">
               products
             </Link>
           </li>
