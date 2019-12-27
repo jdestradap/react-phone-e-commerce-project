e3a6a1f93b3cf4dbbcc81b3b09e1a4b3d22785fa
Juan Diego <jdestradap@gmail.com>
2019-12-27 16:31:34 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -37,13 +37,3 @@ class App extends Component {
   }
 }
 export default App;
-
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150811579-1"></script>
-// <script>
-// window.dataLayer = window.dataLayer || [];
-// function gtag(){dataLayer.push(arguments);}
-// gtag('js', new Date());
-
-// gtag('config', 'UA-150811579-1', { 'optimize_id': 'GTM-59K8QH6'});
-// </script>
